ac770c7ecc8034efd20f01c0882b56d593dc7add
Yaswanth <pyaswanth4@gmail.com>
2022-09-20 16:44:18 +0530

    .

--- MVCWithLinq2/Controllers/StudentController.cs
+++ MVCWithLinq2/Controllers/StudentController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Linq;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,6 +16,62 @@ namespace MVCWithLinq2.Controllers
         {
             List<Student_SelectResult> students = dc.Student_Select(null,true).ToList(); 
             return View(students);
+        }
+        public ViewResult DisplayStudent(int Sid)
+        {
+            Student_SelectResult student = dc.Student_Select(Sid, true).Single();
+            return View(student);
+        }
+        [HttpGet]
+        public ViewResult AddStudent()
+        {
+            return View();
+        }
+        [HttpPost]
+        public RedirectToRouteResult AddStudent(Student_SelectResult student,HttpPostedFileBase selectedFile)
+        {
+            if (selectedFile != null)
+            {
+                string folderPath = Server.MapPath("~/Uploads/");
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+                selectedFile.SaveAs(folderPath + selectedFile.FileName);
+                student.Photo = selectedFile.FileName;
+            }
+            dc.Student_Insert(student.Sid,student.Name,student.Class,student.Fees,student.Photo);
+            return RedirectToAction("DisplayStudents");
+        }
+        public ViewResult EditStudent(int Sid)
+        {
+            Student_SelectResult student = dc.Student_Select(Sid,true).Single();
+            TempData["Photo"] = student.Photo;
+            return View(student);
+        }
+        public RedirectToRouteResult UpdateStudent(Student_SelectResult student,HttpPostedFileBase selectedFile)
+        {
+            if (selectedFile != null)
+            {
+                string folderPath = Server.MapPath("~/Uploads/");
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+                selectedFile.SaveAs(folderPath + selectedFile.FileName);
+                student.Photo = selectedFile.FileName;
+            }
+            else if (TempData["Photo"] !=null)
+            {
+                student.Photo = TempData["Photo"].ToString();
+            }
+            dc.Student_Update(student.Sid, student.Name, student.Class, student.Fees, student.Photo);
+            return RedirectToAction("DisplayStudents");
+        }
+        public RedirectToRouteResult DeleteStudent(int Sid)
+        {
+            dc.Student_Delete(Sid);
+            return RedirectToAction("DisplayStudents");
         }
     }
 }
